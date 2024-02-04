291909d340ec937bcfb61ed2d2ccbdd80c13f8ba
unknown <stevefrisson@gmail.com>
2024-02-04 07:28:16 +0300

    added shadow to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <div className="flex justify-between items-center p-4">
+        <div className="flex justify-between items-center p-4 shadow border-b-[0.5px] border-b-gray-100">
             <div>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 md:w-8 md:h-8">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
@@ -17,8 +17,8 @@ export default function Header() {
                 </div>
                 <div>
                     <div className="flex gap-1 items-center">
-                        <span className="text-xl">admin</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                        <span className="text-xl md:text-2xl">admin</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-[22px] h-[22px] md:w-[26px] md:h-[26px]">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                         </svg>
                     </div>
